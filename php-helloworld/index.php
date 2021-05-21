771748fe011fbe82f7a7af9b8da170afeec320ec
Student User <student@workstation.lab.example.com>
2021-05-21 12:14:20 -0400

    change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,6 @@
 
 <?php
-print "Hosah Alqahtani made the change";
+print "Hosah Alqahtani made the change" "\n";
+print "Hosah's here" "\n";	
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
